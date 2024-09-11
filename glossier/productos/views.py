23fa3a4bb553f1b072764c9ff1913b5ca3d86b5e
coni24 <169170065+coni24@users.noreply.github.com>
2024-09-11 10:48:53 -0300

    1

--- glossier/productos/views.py
+++ glossier/productos/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request,'templateApp/index.html')
+
+def productos(request,categoria):
+    listado = {
+        'balsamo': [
+            {'nombre': "labial"},
+            {'descripcion': "balsamo labial"},
+        ],
+        'serum': [
+            {'nombre': "serum facial"},
+            {'descripcion': "serum hidratante"},
+        ],
+        'mascara': [
+            {'nombre': "mascara de ojos"},
+            {'descripcion': "mascara volumen"},
+        ]}
+    
+    productos = listado.get(categoria, [])
+    return render(request,'templateApp/productos.html', productos)
